45bd348d01adc58f12c9d2f375d1176899fea9cc
Darwin Pineda <darwin.pineda@octapus.io>
2024-09-25 12:19:48 -0500

    Update FullLayout and Router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -21,13 +21,13 @@ import CloudObservability from 'src/views/observability/Cloud';
 import NetworkObservability from 'src/views/observability/Network';
 import Tickets from 'src/views/support/Tickets';
 import AuthGuard from 'src/guards/authGuard/AuthGuard';
+import AiSolution from 'src/views/home/aisolution';
 
-/* ***Layouts**** */
+
 const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
 const BlankLayout = Loadable(lazy(() => import('../layouts/blank/BlankLayout')));
 const Login = Loadable(lazy(() => import('../views/authentication/auth/Login')));
 
-/* ****Pages***** */
 const Router = [
   {
     path: '/',
@@ -37,9 +37,9 @@ const Router = [
       </AuthGuard>
     ),
     children: [
-        // Home
-      { path: '/home/dashboard', element: <Dashboard/> },
+      { path: '/home/dashboard', element: <Dashboard /> },
       { path: '/home/assets', element: <Assets /> },
+      { path: '/aisolution', element: <AiSolution /> },
 
       // Vulnerabilities
       { path: '/vulnerabilities/network', element: <NetworkVulnerabilities /> },
@@ -67,10 +67,9 @@ const Router = [
 
       // Audit
       { path: '/audit/log', element: <Log /> },
+
       // Template Routes
       { path: '/', element: <Navigate to="/home/dashboard" /> },
- 
-
     ],
   },
   {
