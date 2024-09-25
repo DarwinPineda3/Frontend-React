38bcb327c557eb450101faa09ea50ac680315872
Darwin Pineda <darwin.pineda@octapus.io>
2024-09-25 12:30:51 -0500

    Revert FullLayout changes

--- src/layouts/full/FullLayout.tsx
+++ src/layouts/full/FullLayout.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
-import { styled, Container, Box, useTheme, Button } from '@mui/material';
+import { styled, Container, Box, useTheme } from '@mui/material';
 import { useSelector } from 'src/store/Store';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { AppState } from 'src/store/Store';
 import Header from './vertical/header/Header';
 import Sidebar from './vertical/sidebar/Sidebar';
@@ -18,8 +18,8 @@ const MainWrapper = styled('div')(() => ({
 const PageWrapper = styled('div')(() => ({
   display: 'flex',
   flexGrow: 1,
-  flexDirection: 'column',
   paddingBottom: '60px',
+  flexDirection: 'column',
   zIndex: 1,
   width: '100%',
   backgroundColor: 'transparent',
@@ -27,6 +27,7 @@ const PageWrapper = styled('div')(() => ({
 
 const FullLayout: FC = () => {
   const customizer = useSelector((state: AppState) => state.customizer);
+
   const theme = useTheme();
 
   return (
@@ -55,41 +56,15 @@ const FullLayout: FC = () => {
         <Container
           sx={{
             maxWidth: customizer.isLayout === 'boxed' ? 'lg' : '100%!important',
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            minHeight: 'calc(100vh - 170px)', // Ajuste para mantener el espacio
           }}
         >
           {/* ------------------------------------------- */}
           {/* PageContent */}
           {/* ------------------------------------------- */}
-          <Box sx={{ flexGrow: 1 }}>
+
+          <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>
             <Outlet />
           </Box>
-
-          {/* ------------------------------------------- */}
-          {/* Botón para acceder a AiSolution */}
-          {/* ------------------------------------------- */}
-          <Box sx={{ textAlign: 'center', marginTop: 'auto', marginBottom: '20px' }}>
-            <Link to="/aisolution">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                  padding: '10px 20px',
-                  fontSize: '16px',
-                  boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)',
-                  '&:hover': {
-                    backgroundColor: 'primary.dark', 
-                  },
-                }}
-              >
-                Ir a AiSolution
-              </Button>
-            </Link>
-          </Box> 
 
           {/* ------------------------------------------- */}
           {/* End Page */}
