3af34c258d54590cc0f62145460f20953f567caa
Albert Molano <albert.molano@octapus.io>
2024-10-03 01:04:42 -0500

    Refactor: Breadcrumb in web/applications

--- src/views/vulnerabilities/Web/Aplications.tsx
+++ src/views/vulnerabilities/Web/Aplications.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
-import { Box, Grid, IconButton } from "@mui/material";
-import ScanListTable from "src/components/vulnerabilities/web/applications/webScansTable"; // New Detail Component
+import { useParams, useNavigate, useLocation, Link as RouterLink } from 'react-router-dom';
+import { Box, Grid, IconButton, Breadcrumbs, Link, Typography } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ScanListTable from "src/components/vulnerabilities/web/applications/webScansTable"; // New Detail Component
 import AlertDetail from 'src/components/vulnerabilities/web/applications/alertDetail';
 import ScanListDetail from 'src/components/vulnerabilities/web/applications/scanDetail';
 
@@ -52,14 +52,33 @@ const WebApplications = () => {
 
   return (
     <Box>
+      <Box display="flex" alignItems="center" mt={2}>
+        <IconButton onClick={() => navigate(-1)} color="primary">
+          <ArrowBackIcon />
+        </IconButton>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link component={RouterLink} color="inherit" to="/vulnerabilities/web">
+            Vulnerabilidades
+          </Link>
+          <Link component={RouterLink} color="inherit" to="/vulnerabilities/web/applications">
+            Aplicaciones Web
+          </Link>
+          {selectedScan && (
+            <Link component={RouterLink} color="inherit" to={`/vulnerabilities/web/applications/${selectedScan}`}>
+              Escaneos de aplicaciones web
+            </Link>
+          )}
+          {selectedAlert && (
+            <Typography color="textPrimary">
+              Alerta
+            </Typography>
+          )}
+        </Breadcrumbs>
+      </Box>
+
       {/* If a scan is selected and no alert is selected, show scan details */}
       {selectedScan && !selectedAlert ? (
         <Grid container spacing={0} mt={1}>
-          <Grid item xs={12} xl={12}>
-            <IconButton onClick={handleBackToScans} color="primary">
-                <ArrowBackIcon />
-            </IconButton>
-          </Grid>
           <Grid item xs={12} xl={12}>
             <ScanListDetail scanId={selectedScan!} onAlertClick={handleAlertClick} />
           </Grid>
@@ -67,11 +86,6 @@ const WebApplications = () => {
       ) : selectedAlert ? (
         // If an alert is selected, show alert detail
         <Grid container spacing={0} mt={1}>
-          <Grid item xs={12} xl={12}>
-          <IconButton onClick={handleBackToAlerts} color="primary">
-            <ArrowBackIcon />
-            </IconButton>
-          </Grid>
           <Grid item xs={12} xl={12}>
             <AlertDetail alertId={selectedAlert} />
           </Grid>
