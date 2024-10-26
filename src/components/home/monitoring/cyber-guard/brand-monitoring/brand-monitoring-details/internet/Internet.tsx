2331897a7ab2bedc58f9f96e274037084c464449
Andres Ordoñez <aordonez@octapus.io>
2024-10-26 06:32:42 -0500

    fix text not found data

--- src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/internet/Internet.tsx
+++ src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/internet/Internet.tsx
@@ -18,7 +18,7 @@ const Internet: React.FC<InternetProps> = ({ brandMonitoringDetail }) => {
     return (
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Typography variant="h6">No security leaks data available.</Typography>
+          <Typography variant="h6">No internet data available.</Typography>
         </Grid>
       </Grid>
     );
