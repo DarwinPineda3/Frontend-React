727db1154c10c930dbb4a3d20514e35bfa4310bc
Andres Ordoñez <aordonez@octapus.io>
2024-10-30 17:08:11 -0500

    update pagination default (5 -> 25) on tables results (Monitoring)

--- src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/dark-web/DarkWebTable.tsx
+++ src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/dark-web/DarkWebTable.tsx
@@ -23,7 +23,7 @@ interface DarkWebTableProps {
 const DarkWebTable: React.FC<DarkWebTableProps> = ({ dark_web, category }) => {
   const { t } = useTranslation();
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedLeak, setSelectedLeak] = useState<DarkWeb | null>(null);
 
@@ -114,7 +114,7 @@ const DarkWebTable: React.FC<DarkWebTableProps> = ({ dark_web, category }) => {
       </TableContainer>
 
       <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
         component="div"
         count={dark_web.length}
         rowsPerPage={rowsPerPage}
