20bd7c9fa3f8ef3ae37415111ff038cc6291dc34
Andres Ordoñez <aordonez@octapus.io>
2024-11-21 13:31:21 -0500

    feat(vulnerabilities network): add network scan service on list table

--- src/components/vulnerabilities/network/networkScansTable.tsx
+++ src/components/vulnerabilities/network/networkScansTable.tsx
@@ -1,160 +1,167 @@
 import DeleteIcon from '@mui/icons-material/Delete';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import {
-    Box,
-    Chip,
-    Collapse,
-    IconButton,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
+  Box,
+  Chip,
+  IconButton,
+  Pagination,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
 } from '@mui/material';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import DashboardCard from 'src/components/shared/DashboardCard';
-
-// Mock Data
-const scanData = [
-    {
-        id: 1,
-        name: '107.173.154.73',
-        status: 'Done',
-        reports: 3,
-        lastReport: '1 de octubre de 2024 a las 09:08',
-        type: 'Full and fast',
-        scanConfig: 'Configuration 1',
-        targetHosts: '172.16.0.1',
-    },
-    {
-        id: 2,
-        name: 'Escaneo servidor 33',
-        status: 'Done',
-        reports: 2,
-        lastReport: '4 de septiembre de 2024 a las 13:55',
-        type: 'Standard',
-        scanConfig: 'Configuration 2',
-        targetHosts: '172.17.7.33',
-    }
-];
+import { useDispatch, useSelector } from 'src/store/Store';
+import { fetchNetworkScans, setPage } from 'src/store/vulnerabilities/network/NetworkScansSlice';
+import { NetworkScanType } from 'src/types/vulnerabilities/network/networkScansType';
 
 // Collapsible row
-const Row: React.FC<{ row: any, onScanClick: (scanId: number) => void; }> = ({ row, onScanClick }) => {
-    const { t } = useTranslation();
-    const [open, setOpen] = useState(false);
+const Row: React.FC<{ row: any; onScanClick: (scanId: number) => void }> = ({
+  row,
+  onScanClick,
+}) => {
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <TableRow>
-                <TableCell>
-                    <IconButton size="small" onClick={() => setOpen(!open)}>
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell>
-                    <Typography variant="body2">{row.name}</Typography>
-                </TableCell>
-                <TableCell>
-                    <Typography variant="body2">{row.status}</Typography>
-                </TableCell>
-                <TableCell>
-                    <Chip
-                        label={row.reports}
-                        onClick={() => onScanClick(row.id)}
-                        style={{ cursor: 'pointer', color: 'blue' }}
-                        clickable
-                    />
-                </TableCell>
-                <TableCell>
-                    <Typography variant="body2">{row.lastReport}</Typography>
-                </TableCell>
-                <TableCell>
-                    <IconButton color="primary">
-                        <PlayCircleOutlineIcon />
-                    </IconButton>
-                    <IconButton color="error">
-                        <DeleteIcon />
-                    </IconButton>
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell colSpan={6} style={{ paddingBottom: 0, paddingTop: 0 }}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box margin={2}>
-                            <Typography variant="h6">{t("vulnerabilities.detailed_information")}:</Typography>
-                            <Typography variant="body2">
-                                <strong>{t("vulnerabilities.type")}:</strong> {row.type}
-                            </Typography>
-                            <Typography variant="body2">
-                                <strong>{t("vulnerabilities.scan_configuration")}:</strong> {row.scanConfig}
-                            </Typography>
-                            <Typography variant="body2">
-                                <strong>{t("vulnerabilities.target_hosts")}:</strong> {row.targetHosts}
-                            </Typography>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </>
-    );
+  return (
+    <>
+      <TableRow>
+        {/* <TableCell>
+          <IconButton size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell> */}
+        <TableCell>
+          <Typography variant="body2">{row.name}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body2">{row.status}</Typography>
+        </TableCell>
+        <TableCell>
+          <Chip
+            label={row.report_count}
+            onClick={() => onScanClick(row.id_elastic)}
+            style={{ cursor: 'pointer', color: 'blue' }}
+            clickable
+          />
+        </TableCell>
+        <TableCell>
+          <Typography variant="body2">{row.last_report}</Typography>
+        </TableCell>
+        <TableCell>
+          <IconButton color="primary">
+            <PlayCircleOutlineIcon />
+          </IconButton>
+          <IconButton color="error">
+            <DeleteIcon />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+      {/* <TableRow>
+        <TableCell colSpan={6} style={{ paddingBottom: 0, paddingTop: 0 }}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={2}>
+              <Typography variant="h6">{t('vulnerabilities.detailed_information')}:</Typography>
+              <Typography variant="body2">
+                <strong>{t('vulnerabilities.type')}:</strong> {row.type}
+              </Typography>
+              <Typography variant="body2">
+                <strong>{t('vulnerabilities.scan_configuration')}:</strong> {row.scanConfig}
+              </Typography>
+              <Typography variant="body2">
+                <strong>{t('vulnerabilities.target_hosts')}:</strong> {row.targetHosts}
+              </Typography>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow> */}
+    </>
+  );
 };
 
 interface NetworkScanTableProps {
-    onScanClick: (scanId: number) => void;
+  onScanClick: (scanId: number) => void;
 }
 
 const NetworkScanTable: React.FC<NetworkScanTableProps> = ({ onScanClick }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const networkScans = useSelector((state: any) => state.networkScanReducer.networkScans);
+  const currentPage = useSelector((state: any) => state.networkScanReducer.page);
+  const totalPages = useSelector((state: any) => state.networkScanReducer.totalPages);
 
-    return (
-        <DashboardCard title={t("vulnerabilities.network_scans")!} subtitle={t("vulnerabilities.network_scans_subtitle")!}>
-            <TableContainer component={Paper}>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell />
-                            <TableCell>
-                                <Typography variant="subtitle2" fontWeight={600}>
-                                    {t("vulnerabilities.name")}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography variant="subtitle2" fontWeight={600}>
-                                    {t("vulnerabilities.status")}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography variant="subtitle2" fontWeight={600}>
-                                    {t("vulnerabilities.reports")}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography variant="subtitle2" fontWeight={600}>
-                                    {t("vulnerabilities.last_report")}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography variant="subtitle2" fontWeight={600}>
-                                    {t("vulnerabilities.actions")}
-                                </Typography>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {scanData.map((row) => (
-                            <Row key={row.id} row={row} onScanClick={onScanClick} />
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </DashboardCard>
-    );
+  React.useEffect(() => {
+    dispatch(fetchNetworkScans(currentPage));
+  }, [dispatch, currentPage]);
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    if (page !== currentPage) {
+      dispatch(setPage(page));
+    }
+  };
+
+  return (
+    <DashboardCard
+      title={t('vulnerabilities.network_scans')!}
+      subtitle={t('vulnerabilities.network_scans_subtitle')!}
+    >
+      <Box>
+        <TableContainer component={Paper}>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                {/* <TableCell /> */}
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    {t('vulnerabilities.name')}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    {t('vulnerabilities.status')}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    {t('vulnerabilities.reports')}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    {t('vulnerabilities.last_report')}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="subtitle2" fontWeight={600}>
+                    {t('vulnerabilities.actions')}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {networkScans.map((row: NetworkScanType) => (
+                <Row key={row.id} row={row} onScanClick={onScanClick} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box my={3} display="flex" justifyContent={'center'}>
+          <Pagination
+            count={totalPages}
+            color="primary"
+            page={currentPage}
+            onChange={handlePageChange}
+          />
+        </Box>
+      </Box>
+    </DashboardCard>
+  );
 };
 
 export default NetworkScanTable;
