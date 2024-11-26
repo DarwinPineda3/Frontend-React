459938da1b46388ac78caf6dbb6055ca51dad37c
Andres Ordoñez <aordonez@octapus.io>
2024-11-25 20:25:26 -0500

    feat(vulnerabilities network): update NetworkScanTable main component

--- src/components/vulnerabilities/network/networkScansTable.tsx
+++ src/components/vulnerabilities/network/networkScansTable.tsx
@@ -1,5 +1,8 @@
+import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import StopIcon from '@mui/icons-material/Stop';
 import {
   Box,
   Chip,
@@ -16,11 +19,12 @@ import {
 } from '@mui/material';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import DashboardCard from 'src/components/shared/DashboardCard';
+import HumanizedDate from 'src/components/shared/HumanizedDate';
 import { useDispatch, useSelector } from 'src/store/Store';
 import { fetchNetworkScans, setPage } from 'src/store/vulnerabilities/network/NetworkScansSlice';
 import { NetworkScanType } from 'src/types/vulnerabilities/network/networkScansType';
-
 // Collapsible row
 const Row: React.FC<{ row: any; onScanClick: (scanId: number) => void }> = ({
   row,
@@ -28,6 +32,25 @@ const Row: React.FC<{ row: any; onScanClick: (scanId: number) => void }> = ({
 }) => {
   const { t } = useTranslation();
   const [open, setOpen] = useState(false);
+
+  const handleActionClick = (action: string, scanId: number) => {
+    switch (action) {
+      case 'run':
+        // dispatch(startScan(scanId));
+        break;
+      case 'stop':
+        // dispatch(stopScan(scanId));
+        break;
+      case 'resume':
+        // dispatch(resumeScan(scanId));
+        break;
+      case 'delete':
+        // dispatch(deleteScan(scanId));
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <>
@@ -39,26 +62,67 @@ const Row: React.FC<{ row: any; onScanClick: (scanId: number) => void }> = ({
         </TableCell> */}
         <TableCell>
           <Typography variant="body2">{row.name}</Typography>
+          {row.comment && (
+            <Typography variant="caption" color="textSecondary">
+              ({row.comment})
+            </Typography>
+          )}
         </TableCell>
         <TableCell>
-          <Typography variant="body2">{row.status}</Typography>
+          {row.status === 'Running' ? (
+            <>
+              <Typography variant="body2">
+                {row.status} - {row.progress}%
+              </Typography>
+              <Typography variant="caption" color="textSecondary">
+                ({row.current_report})
+              </Typography>
+            </>
+          ) : (
+            <Typography variant="body2">{row.status}</Typography>
+          )}
         </TableCell>
         <TableCell>
           <Chip
-            label={row.report_count}
-            onClick={() => onScanClick(row.id_elastic)}
-            style={{ cursor: 'pointer', color: 'blue' }}
-            clickable
+            label={
+              row.status !== 'New' ? row.report_count || row.report_count?.total || 'N/A' : '0'
+            }
+            onClick={row.status !== 'New' ? () => onScanClick(row.id_elastic) : undefined}
+            style={{
+              cursor: row.status !== 'New' ? 'pointer' : 'default',
+              color: row.status !== 'New' ? 'blue' : 'gray',
+            }}
+            clickable={row.status !== 'New'}
           />
         </TableCell>
         <TableCell>
-          <Typography variant="body2">{row.last_report}</Typography>
+          <Typography variant="body2">
+            {row.status !== 'New' ? <HumanizedDate dateString={row.last_report} /> : ''}
+          </Typography>
         </TableCell>
         <TableCell>
-          <IconButton color="primary">
-            <PlayCircleOutlineIcon />
-          </IconButton>
-          <IconButton color="error">
+          {row.status === 'Running' || row.status === 'Queued' ? (
+            <IconButton color="primary" onClick={() => handleActionClick('stop', row.id_elastic)}>
+              <StopIcon />
+            </IconButton>
+          ) : row.status !== 'Requested' ? (
+            <>
+              <IconButton color="primary" onClick={() => handleActionClick('run', row.id_elastic)}>
+                <PlayCircleOutlineIcon />
+              </IconButton>
+
+              {row.status === 'Stopped' && (
+                <IconButton
+                  color="primary"
+                  onClick={() => handleActionClick('resume', row.id_elastic)}
+                >
+                  <PlayArrowIcon />
+                </IconButton>
+              )}
+            </>
+          ) : null}
+
+          <IconButton color="error" onClick={() => handleActionClick('delete', row.id_elastic)}>
             <DeleteIcon />
           </IconButton>
         </TableCell>
@@ -92,6 +156,7 @@ interface NetworkScanTableProps {
 const NetworkScanTable: React.FC<NetworkScanTableProps> = ({ onScanClick }) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const networkScans = useSelector((state: any) => state.networkScanReducer.networkScans);
   const currentPage = useSelector((state: any) => state.networkScanReducer.page);
   const totalPages = useSelector((state: any) => state.networkScanReducer.totalPages);
@@ -106,10 +171,20 @@ const NetworkScanTable: React.FC<NetworkScanTableProps> = ({ onScanClick }) => {
     }
   };
 
+  const handleEditClick = () => {
+    navigate('/vulnerabilities/network/scans/create');
+  };
+  const addButton = (
+    <IconButton color="primary" onClick={() => handleEditClick()}>
+      <AddIcon />
+    </IconButton>
+  );
+
   return (
     <DashboardCard
       title={t('vulnerabilities.network_scans')!}
       subtitle={t('vulnerabilities.network_scans_subtitle')!}
+      action={addButton}
     >
       <Box>
         <TableContainer component={Paper}>
