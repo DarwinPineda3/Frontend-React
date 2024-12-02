65beea5ee3fc0e891a8aa4e77a17b61d8dc9f593
Andres Ordoñez <aordonez@octapus.io>
2024-12-02 08:58:04 -0500

    feat(vulnerabilities network): add new types for complete detail

--- src/types/vulnerabilities/network/networkScansType.ts
+++ src/types/vulnerabilities/network/networkScansType.ts
@@ -125,3 +125,236 @@ export interface Scan {
   is_active: boolean;
   id_elastic: string;
 }
+
+export interface GraphicsChartsData {
+  labels: string[];
+  values: number[];
+}
+
+interface Nvt {
+  oid: string;
+  type: string;
+  name: string;
+  family: string;
+  cvss_base: string;
+  severities: Severity[];
+  tags: Tags;
+  solution: Solution;
+  refs: any[];
+}
+
+interface Severity {
+  severity: CvssBaseSeverity;
+}
+
+interface CvssBaseSeverity {
+  type: string;
+  origin: string | null;
+  date: string;
+  score: string;
+  value: string;
+}
+
+interface Tags {
+  cvss_base_vector: string;
+  summary: string;
+  insight: string;
+  affected: string;
+  impact: string;
+  solution: string;
+  vuldetect: string;
+  solution_type: string;
+}
+
+interface Solution {
+  type: string;
+  id: string;
+}
+
+interface Qod {
+  value: string;
+  type: string | null;
+}
+
+interface Host {
+  host: string;
+  asset: AssetN;
+  hostname: string | null;
+  details: Detail[];
+}
+
+interface Detail {
+  name: string;
+  value: string;
+  source: {
+    type: string;
+    name: string;
+    description: string | null;
+  };
+  extra: string | null;
+}
+
+interface Result {
+  id: string;
+  name: string;
+  modification_time: string;
+  comment: string | null;
+  creation_time: string;
+  host: Host;
+  port: string;
+  nvt: Nvt;
+  scan_nvt_version: string;
+  severity: number;
+  threat: string;
+  qod: Qod;
+  description: string;
+  original_threat: string;
+  original_severity: string;
+  report_date: string;
+  creation_time_format?: string;
+}
+
+interface ResultN {
+  result: Result;
+}
+
+interface ListOS {
+  result: Result;
+}
+
+interface Task {
+  id: string;
+  name: string;
+}
+
+interface TaskN {
+  id: string;
+  name: string;
+  comment: string | null;
+  target: {
+    id: string;
+    name: string;
+    comment: string | null;
+  };
+  id_elastic: string;
+  hosts: string;
+}
+
+interface PortDetails {
+  ports: Port[];
+}
+
+interface Port {
+  port: PortInfo;
+}
+
+interface PortInfo {
+  port: string;
+  host: string;
+  severity: number;
+  threat: string;
+}
+
+interface ResultCount {
+  full: string;
+  filtered: string;
+  hole: CountDetails;
+  info: CountDetails;
+  warning: CountDetails;
+  false_positive: CountDetails;
+}
+
+interface CountDetails {
+  full: string;
+  filtered: string;
+}
+
+interface ErrorCount {
+  count: string;
+}
+
+interface HostList {
+  ip: string;
+  asset: AssetN;
+  start: string;
+  end: string;
+  result_count: ResultCountPage;
+}
+
+interface AssetN {
+  asset_id: string;
+}
+
+interface ResultCountPage {
+  page: string;
+  hole: CountDetailsPage;
+  warning: CountDetailsPage;
+  info: CountDetailsPage;
+  log: CountDetailsPage;
+  false_positive: CountDetailsPage;
+}
+
+interface CountDetailsPage {
+  page: string;
+}
+
+interface ReportS {
+  scan_run_status: string;
+  hosts: {
+    count: string;
+  };
+  closed_cves: string;
+  vulns: {
+    count: string;
+  };
+  os: {
+    count: number;
+  };
+  apps: {
+    count: number;
+  };
+  ssl_certs: string;
+  task: TaskN;
+  timestamp: string;
+  scan_start: string;
+  timezone: string;
+  timezone_abbrev: string;
+  ports: PortDetails[];
+  result_count: ResultCount;
+  severity: ResultCount;
+  scan_end: string;
+  errors: ErrorCount;
+  host_list: HostList[];
+  results: ResultN[];
+  vulnerabilities: ResultN[];
+  scan_end_format: string;
+}
+
+interface Statistics {
+  critical: number;
+  high: number;
+  medium: number;
+  low: number;
+  count_vulnerabilities: number;
+}
+
+interface Report {
+  id: string;
+  name: string;
+  comment: string | null;
+  creation_time: string;
+  modification_time: string;
+  task: Task;
+  report: ReportS;
+  statistics: Statistics;
+  is_last_report: boolean;
+  elastic_id: string;
+}
+
+export interface ReportDetail {
+  task_id: string;
+  report: Report;
+  report_detail_chart_data: GraphicsChartsData;
+  list_os: ListOS[];
+  id_elastic: string;
+}
