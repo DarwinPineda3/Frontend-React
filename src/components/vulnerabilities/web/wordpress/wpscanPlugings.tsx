234105b6cd7a86baf62ec721d947474a67bacee4
Andres M <amunoz@2secure.co>
2024-12-03 11:57:10 -0500

    translate

--- src/components/vulnerabilities/web/wordpress/wpscanPlugings.tsx
+++ src/components/vulnerabilities/web/wordpress/wpscanPlugings.tsx
@@ -1,3 +1,5 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import WarningIcon from '@mui/icons-material/Warning';
 import {
   Box,
   Table,
@@ -15,6 +17,17 @@ import HumanizedDate from 'src/components/shared/HumanizedDate';
 const WPSPlugins: React.FC<{ plugins_list: any[] }> = ({ plugins_list }) => {
   const { t } = useTranslation();
 
+  const renderVulnerabilities = (vulnerabilities: any) => {
+    if (vulnerabilities.length === 0) {
+      return <span>{t('wpscan.no_vulnerabilities')}</span>;
+    }
+
+    return vulnerabilities.map((vul: any, index: any) => (
+      <Box key={index}>
+        <p>{vul}</p>
+      </Box>
+    ));
+  };
 
   return (
     <TableContainer>
@@ -22,7 +35,7 @@ const WPSPlugins: React.FC<{ plugins_list: any[] }> = ({ plugins_list }) => {
         <TableHead>
           <TableRow>
             <TableCell>
-              <Typography variant="subtitle2" fontWeight={600}> {/* colocar modal para ver más detalles */}
+              <Typography variant="subtitle2" fontWeight={600}>
                 {t('wpscan.plugin_name')}
               </Typography>
             </TableCell>
@@ -41,6 +54,11 @@ const WPSPlugins: React.FC<{ plugins_list: any[] }> = ({ plugins_list }) => {
                 {t('wpscan.last_update')}
               </Typography>
             </TableCell>
+            <TableCell>
+              <Typography variant="subtitle2" fontWeight={600}>
+                {t('wpscan.vulnerabilities')}
+              </Typography>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -54,13 +72,23 @@ const WPSPlugins: React.FC<{ plugins_list: any[] }> = ({ plugins_list }) => {
               <TableCell>
                 <Box display="flex" alignItems="center" gap={1}>
                   <Typography variant="body2">{plugin.version?.number}</Typography>
+                  {plugin.outdated ? (
+                    <WarningIcon color="error" fontSize="small" />
+                  ) : (
+                    <CheckCircleIcon color="success" fontSize="small" />
+                  )}
                 </Box>
               </TableCell>
               <TableCell>
                 <Typography variant="body2">{plugin?.latest_version}</Typography>
               </TableCell>
               <TableCell>
-                <Typography variant="body2"><HumanizedDate dateString={plugin?.last_updated} /></Typography>
+                <Typography variant="body2">
+                  <HumanizedDate dateString={plugin?.last_updated} />
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body2">{renderVulnerabilities(plugin?.vulnerabilities)}</Typography>
               </TableCell>
             </TableRow>
           ))}
