3a2230bbf35e87cf40f138cbef910b3ccfe211e3
Andres Ordoñez <aordonez@octapus.io>
2024-12-10 10:06:02 -0500

    fix(management vulnerabilities): bugs

--- src/store/vulnerabilities/ManagementVulnSlice.tsx
+++ src/store/vulnerabilities/ManagementVulnSlice.tsx
@@ -186,7 +186,9 @@ export const downloadEvidence = (id: number) => async (dispatch: AppDispatch) =>
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', response.headers['content-disposition'].split('filename=')[1]);
+    const contentDisposition = response.headers['content-disposition'];
+    const fileName = contentDisposition.split('filename=')[1].replace(/['"]/g, '');
+    link.setAttribute('download', fileName);
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -217,6 +219,7 @@ export const downloadVulnerabilitiesReport =
       const fileName = contentDisposition
         ? contentDisposition.split('filename=')[1].replace(/['"]/g, '')
         : 'ManagedVulnerabilities.xlsx';
+      console.log(fileName);
 
       link.setAttribute('download', fileName);
 
