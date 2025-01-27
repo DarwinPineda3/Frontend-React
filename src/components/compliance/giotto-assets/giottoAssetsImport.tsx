4f773455358b6a1597ca4dcf54e068e240d14c13
Andres M <amunoz@2secure.co>
2025-01-27 17:38:51 -0500

    template for bulk import assets and translates

--- src/components/compliance/giotto-assets/giottoAssetsImport.tsx
+++ src/components/compliance/giotto-assets/giottoAssetsImport.tsx
@@ -1,14 +1,15 @@
 import { ArrowBack } from "@mui/icons-material";
-import { Box, Breadcrumbs, Button, Chip, Divider, IconButton, Link, Typography, useTheme } from "@mui/material";
+import { Box, Breadcrumbs, Button, Chip, Divider, IconButton, Link, Tooltip, Typography, useTheme } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { useTranslation } from "react-i18next";
 // import { useNavigate } from "react-router";
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import PageContainer from "src/components/container/PageContainer";
 import DashboardCard from "src/components/shared/DashboardCard";
 import { uploadAssets } from "src/store/sections/compliance/giotoAssetsSlice";
 import { useDispatch, useSelector } from "src/store/Store";
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
 const GiottoAssetsImport: React.FC = () => {
   const theme = useTheme();
@@ -64,14 +65,32 @@ const GiottoAssetsImport: React.FC = () => {
       <Typography variant="body1" fontWeight="500">
         {file.name}{" "}
       </Typography>
-      <Chip color="primary" label={`${file.size} Bytes`} />
-      {/*remove file*/}
+      <Chip color="primary" label={`${(file.size / 1024).toFixed(2)} KB`} />
       <Chip
-        label="Remove"
+        label={t('giotto.assets.remove')}
         color="error"
         onClick={() => removeFile()} />
     </Box>
   ));
+
+  const addButton = (
+    <Box>
+      <Tooltip title={t('giotto.assets.download_template')} arrow>
+        <IconButton color="primary" onClick={() => handleHelpToBulkImportClick()}>
+          <HelpOutlineIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+
+  const handleHelpToBulkImportClick = () => {
+    const fileUrl = '/assets/template-bulk-import.csv';
+
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'template.csv';
+    link.click();
+  };
 
   return (
     <PageContainer title={String(t('compliance_menu.compliance_assets'))}>
@@ -91,13 +110,14 @@ const GiottoAssetsImport: React.FC = () => {
           </Breadcrumbs>
         </Box>
       </Box>
-    <Box>
-      <DashboardCard title="Import Giotto Assets">
+      <Box>
 
-        {/*file drop zone*/}
-        <Box>
+        <DashboardCard
+          title={t('giotto.assets.import_giotto_assets') || ''}
+          action={addButton}
+        >
+
           <Box>
-
             <Box
               mt={3}
               fontSize="12px"
@@ -113,6 +133,7 @@ const GiottoAssetsImport: React.FC = () => {
             >
               <input {...getInputProps()} />
               <Typography>{t('giotto.assets.select_file')}</Typography>
+              <Typography fontSize="0.9em">{t('giotto.assets.select_csv_file')}</Typography>
             </Box>
             <Box mt={2}>
               <Typography variant="h6" fontSize="15px">
@@ -120,26 +141,25 @@ const GiottoAssetsImport: React.FC = () => {
               </Typography>
               <Box>{files}</Box>
             </Box>
+            {
+              myFiles.length > 0 && <Divider />
+            }
+            {
+              myFiles.length > 0 && <Box sx={{ pt: 3, display: "flex", justifyContent: "end" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => uploadFile(myFiles)}
+                >
+                  {t('giotto.assets.upload')}
+                </Button>
+              </Box>
+            }
+
           </Box>
-          {
-            myFiles.length > 0 && <Divider />
-          }
-          {
-            myFiles.length > 0 && <Box sx={{ pt: 3, display: "flex", justifyContent: "end" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={(e) => uploadFile(myFiles)}
-              >
-                {"Upload"}
-              </Button>
-            </Box>
-          }
 
-        </Box>
-
-      </DashboardCard>
-    </Box>
+        </DashboardCard>
+      </Box>
     </PageContainer>
   );
 };
