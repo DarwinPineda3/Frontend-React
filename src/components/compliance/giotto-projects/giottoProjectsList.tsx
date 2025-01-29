c45e8ac215865ecafd3ef7c1b3e93d2e0e72849b
jcordoba <jcordoba@2secure.co>
2025-01-29 12:34:50 -0500

    Fix Compliance Projects Pagination

--- src/components/compliance/giotto-projects/giottoProjectsList.tsx
+++ src/components/compliance/giotto-projects/giottoProjectsList.tsx
@@ -142,8 +142,15 @@ const GiottoProjectsList = () => {
               <TableBody>
                 {loading ? (
                   <TableRow>
-                    <TableCell colSpan={5}>
-                      <Loader />
+                    <TableCell colSpan={6}>
+                      <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        height="100px"
+                      >
+                        <Loader />
+                      </Box>
                     </TableCell>
                   </TableRow>
                 ) : (
