4861ab446427c7f6e6c6735c24d1daab578e418b
Andres Ordoñez <aordonez@octapus.io>
2025-01-29 18:53:37 -0500

    feat(schedule-scans): add types and enumerations for scheduled scan configuration

---
+++ src/types/schedule-scans-settings/schedule_scans_type.ts
@@ -0,0 +1,52 @@
+export enum ExecutionFrequencies {
+  EVERY_DAY = 1,
+}
+
+export enum ExecutionStatuses {
+  SCHEDULED = 'SCHEDULED',
+  EXECUTING = 'EXECUTING',
+  COMPLETED = 'COMPLETED',
+  ERROR = 'ERROR',
+}
+
+export interface AssetType {
+  id: number;
+  name: string;
+  ip: string | null;
+  domain: string | null;
+  url: string | null;
+  hostname: string;
+  uuid: string;
+  created_date: string;
+  updated_date: string;
+}
+
+export interface ScheduledTaskType {
+  id: number;
+  name: string;
+  scan_type: number;
+  asset: AssetType;
+  execution_frequency: ExecutionFrequencies;
+  execution_time: string;
+  created_date: string;
+  updated_date: string;
+  is_active: boolean;
+}
+
+export interface ScheduledExecutionType {
+  id: number;
+  scheduled_date: string;
+  scheduled_status: ExecutionStatuses;
+  created_date: string;
+  updated_date: string;
+  elastic_id: string | null;
+  report_id: string | null;
+  openvas_task_id: string | null;
+  elastic_task_id: string | null;
+  scheduled_task: number;
+}
+
+export interface ScheduledScanDetail {
+  scheduled_scan: ScheduledTaskType;
+  scheduled_executions: ScheduledExecutionType[];
+}
