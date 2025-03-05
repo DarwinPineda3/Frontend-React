a2b304171e35d65aef1715f8af9d3b715ca3986d
jcordoba <jcordoba@2secure.co>
2025-03-05 15:13:10 -0500

    Fix brand monitoring details

--- src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/internet/InternetIndicators.tsx
+++ src/components/home/monitoring/cyber-guard/brand-monitoring/brand-monitoring-details/internet/InternetIndicators.tsx
@@ -4,6 +4,7 @@ import DataObjectIcon from '@mui/icons-material/DataObject';
 import CodeIcon from '@mui/icons-material/Code';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import LinkIcon from '@mui/icons-material/Link';
+import PasswordIcon from '@mui/icons-material/Password';
 import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -22,6 +23,7 @@ interface InternetIndicatorsProps {
     interesting_files: number;
     public_code_repo: number;
     raw_file_meta_data: number;
+    url_password: number;
   };
 }
 
@@ -89,6 +91,11 @@ const InternetIndicators: React.FC<InternetIndicatorsProps> = ({ internetCounter
       title: t('monitoring.raw_file_meta_data'),
       value: internetCounters.raw_file_meta_data,
       icon: <DataObjectIcon fontSize="large" />,
+    },
+    {
+      title: t('monitoring.url_password'),
+      value: internetCounters.url_password,
+      icon: <PasswordIcon fontSize="large" />,
     },
   ];
 
